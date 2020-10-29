62348074d8951085ada74efac125c761b0d9944a
PaxPrz <paxprajapati@gmail.com>
2020-10-29 09:42:11 +0545

    exception

--- __init__.py
+++ __init__.py
@@ -1,8 +1,14 @@
 import sys
+try:
+    from utils.exceptions import PlatformNotSupported
+except ImportError:
+    from .utils.exceptions import PlatformNotSupported
 
 platform = sys.platform
 
 if platform.startswith('win'):
-    from .windetector import get_window_and_url, get_url
-else:
+    from .windetector import get_window_and_url, get_url, get_window_name
+elif platform.startswith('linux'):
     from .xlibdetector import get_url, get_window_and_url, get_window_name
+else:
+    raise PlatformNotSupported(platform)
